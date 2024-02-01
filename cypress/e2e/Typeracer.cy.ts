ab1156dd3fdf9d6df104e5706577c2dd36e6a817
Jannick Bath <jannick.bath@lupcom.de>
2024-02-01 18:27:24 +0100

    +tests

--- cypress/e2e/Typeracer.cy.ts
+++ cypress/e2e/Typeracer.cy.ts
@@ -47,34 +47,40 @@ describe('Typeracer', () => {
     /**
      * Checks if all components are rendered and visible.
      */
-    // it("Components render", () => {
-    //     cy.get("app-textbox").should("exist").should("be.visible");
-    //     cy.get("app-controls").should("exist").should("be.visible");
-    //     cy.get("app-utility").should("exist").should("be.visible");
-    //     cy.get("app-open-setup").should("exist").should("be.visible");
-    //     cy.get("app-open-bots").should("exist").should("be.visible");
-    //     cy.get("app-tracks").should("exist").should("be.visible");
-    //     cy.get("app-car").should("exist").should("be.visible");
-    // })
+    it("Components render", () => {
+        cy.get("app-textbox").should("exist").should("be.visible");
+        cy.get("app-controls").should("exist").should("be.visible");
+        cy.get("app-utility").should("exist").should("be.visible");
+        cy.get("app-open-setup").should("exist").should("be.visible");
+        cy.get("app-open-bots").should("exist").should("be.visible");
+        cy.get("app-tracks").should("exist").should("be.visible");
+        cy.get("app-car").should("exist").should("be.visible");
+    })
 
-    // it("Has working Bots", () => {
-    //   cy.get(".game-state-toggle").click();
-    //   cy.wait(1000);
+    /**
+     * Checks if bots are moving and progress is updated.
+     */
+    it("Has working Bots", () => {
+      cy.get(".game-state-toggle").click();
+      cy.wait(1000);
       
-    //   cy.get('.car-default').should((el) => {
-    //     const numericValueOfLeftProperty = parseFloat(el.css("left"));
-    //     expect(numericValueOfLeftProperty).to.be.greaterThan(0); // 0%
-    //   });
+      cy.get('.car-default').should((el) => {
+        const numericValueOfLeftProperty = parseFloat(el.css("left"));
+        expect(numericValueOfLeftProperty).to.be.greaterThan(0); // 0%
+      });
 
-    //   cy.get(".game-state-toggle").click();
-    //   cy.wait(200);
+      cy.get(".game-state-toggle").click();
+      cy.wait(200);
 
-    //   cy.get('.car-default').should((el) => {
-    //     const numericValueOfLeftProperty = parseFloat(el.css("left"));
-    //     expect(numericValueOfLeftProperty).to.be.equal(0); // 0%
-    //   });
-    // });
+      cy.get('.car-default').should((el) => {
+        const numericValueOfLeftProperty = parseFloat(el.css("left"));
+        expect(numericValueOfLeftProperty).to.be.equal(0); // 0%
+      });
+    });
 
+    /**
+     * Checks if textbox displays correct keypress. And if progress is updated accordingly.
+     */
     it('Handles correct key press', () => {
       cy.get(".game-state-toggle").click();
       cy.wait(300);
@@ -82,6 +88,7 @@ describe('Typeracer', () => {
       useTextBoxContent(text => {
         const correctKey = text[0];
         focusAndPressKeyInTextbox(correctKey);
+        cy.wait(100);
 
         cy.get('.textbox span.correct').last()
           .should('have.text', correctKey)
@@ -95,6 +102,9 @@ describe('Typeracer', () => {
       cy.get(".game-state-toggle").click();  
     });
   
+    /**
+     * Checks if textbox shows invalid keypress and does not update progress.
+     */
     it('Handles incorrect key press', () => {
       cy.get(".game-state-toggle").click();
       cy.wait(200);
@@ -102,6 +112,7 @@ describe('Typeracer', () => {
       useTextBoxContent(text => {
         const incorrectKey = text[0] == "z" ? "x" : "z";
         focusAndPressKeyInTextbox(incorrectKey);
+        cy.wait(100);
         
         cy.get('.textbox span.invalid').last()
           .should("exist")
@@ -115,80 +126,20 @@ describe('Typeracer', () => {
       cy.get(".game-state-toggle").click();
     });
   
-    // it('Handles backspace key press', () => {
-    //   cy.wait(400);
-    //   useTextBoxContent(text => {
-    //     const incorrectKey = text[0] == "z" ? "x" : "z";
-    //     focusAndPressKeyInTextbox(incorrectKey);
-    //     cy.get('.input-div').type('{backspace}');
-    //     cy.get('.previous-text span').should("not.exist");
-    //   });
-    // });
-
-    // it("Can handle opponent generation", () => {
-    //   cy.get(".car").should("have.length", 1);
-    //   cy.get(".generate-opponents").click(); // 2
-    //   cy.get(".car").should("have.length", 2);
-
-    //   // Check if max amount can be exceeded
-    //   cy.get(".generate-opponents").click(); // 3
-    //   cy.get(".generate-opponents").click(); // 4
-    //   cy.get(".generate-opponents").click(); // 5
-    //   cy.get(".car").should("have.length", 4);
-    // });
-
-    // it("Has a working player car", () => {
-    //   cy.wait(200);
-    //   cy.get(".start-button").click();
-
-    //   cy.get('.car.own').should((el) => {
-    //     const numericValueOfLeftProperty = parseFloat(el.css("left"));
-    //     expect(numericValueOfLeftProperty).to.be.eq(0); // 0%
-    //   });
-
-    //   // Check for player car movement
-    //   useTextBoxContent(text => {
-    //     for (let i = 0; i < text.length; i++) {
-    //       const correctKey = text[i];
-    //       focusAndPressKeyInTextbox(correctKey);
-    //     }
-    //     focusAndPressKeyInTextbox(" "); // Workaround for current bug
-
-    //     cy.get('.popup.winner').should("exist");
-    //     cy.get(".popup.winner h2").should("contain.text", "Player");
-    //     cy.get('.popup.winner .close-button').click();
-
-    //     // Check if the automatic reset works. (After the game is over)
-    //     cy.get('.car.own').should((el) => {
-    //       const numericValueOfLeftProperty = parseFloat(el.css("left"));
-    //       expect(numericValueOfLeftProperty).to.be.eq(0); // 0%
-    //     });
-    //   });
-    // });
-
-    // it("Has a working restart button", () => {
-    //   const gameToggleButton = cy.get(".start-button");
-    //   cy.wait(400);
-    //   gameToggleButton.click(); // start
+    /**
+     * Checks if the textbox handles backspace input correctly.
+     */
+    it('Handles backspace key press', () => {
+      cy.get(".game-state-toggle").click();
+      cy.wait(200);
       
-    //   useTextBoxContent(text => {
-    //     for (let i = 0; i < 10; i++) {
-    //       const correctKey = text[i];
-    //       focusAndPressKeyInTextbox(correctKey);
-    //     }
-    //   });
-
-    //   cy.get('.car.own').should((el) => {
-    //     const numericValueOfLeftProperty = parseFloat(el.css("left"));
-    //     expect(numericValueOfLeftProperty).to.be.above(0); // > 0%
-    //   });
-
-    //   gameToggleButton.click(); // restart
-      
-    //   cy.get('.car.own').should((el) => {
-    //     const numericValueOfLeftProperty = parseFloat(el.css("left"));
-    //     expect(numericValueOfLeftProperty).to.be.eq(0); // == 0%
-    //   });
-    // })
+      useTextBoxContent(text => {
+        const correctKey = text[0];
+        focusAndPressKeyInTextbox(correctKey);
+        cy.get('.textbox').type('{backspace}');
+        cy.get('.textbox span.correct').should("not.exist");
+      });
+      cy.get(".game-state-toggle").click();
+    });
 });
   
